5ae74eeeda93eb7b03bac538f78dd504663d4563
camila.scarneiro1 <camila.scarneiro1@CAR0588302W10-1>
2023-08-18 13:51:57 -0300

    Adicionar arquivos de projeto.

---
+++ Fase2-Aula12-Atividade/Produto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fase2_Aula12_Atividade
+{
+    internal class Produto
+    {
+        public string nome;
+        public string descricao;
+        public int quantidade;
+        public double preco;
+        public bool status;
+
+
+        public void mostra()
+        {
+            Console.WriteLine($"Seu produto é {nome} ele é {descricao}, tem {quantidade}, o valor é {preco} e esta {status}");
+        }
+
+        public void desativa()
+        {
+            quantidade = 0;
+            status= false; 
+           
+               
+        }
+
+        
+        
+    }
+}
---
+++ Fase2-Aula12-Atividade/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using Fase2_Aula12_Atividade;
+
+Console.WriteLine("Hello, World!");
+
+Produto produto1 = new Produto();
+
+
+
+produto1.nome = "Salgado";
+produto1.descricao = "Frango com catupiry";
+produto1.quantidade = 1;
+produto1.preco = 4.00;
+produto1.status = true;
+produto1.desativa();
+produto1.mostra();
+
+
+Produto produto2 = new Produto();
+
+
+produto2.nome = "Bolo";
+produto2.descricao = "Leite Ninho";
+produto2.quantidade = 0;
+produto2.preco = 12.00;
+produto2.status = true;
+produto2.desativa();
+produto2.mostra();
+
+
+
+Produto produto3 = new Produto();
+
+
+produto3.nome = "Brigadeiro";
+produto3.descricao = "Chocolate";
+produto3.quantidade = 5;
+produto3.preco = 2.00;
+produto3.status = true;
+
+produto3.mostra();
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadKey();
